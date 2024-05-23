8b49c4dac444cc8f137d90549ca0e532a1f993af
VoThanhThe <vothanhthepct2304@gmail.com>
2024-05-23 23:14:00 +0700

    ok

--- ios/Pods/Target Support Files/Pods-TaskLogin-TaskLoginTests/Pods-TaskLogin-TaskLoginTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TaskLogin_TaskLoginTests : NSObject
-@end
-@implementation PodsDummy_Pods_TaskLogin_TaskLoginTests
-@end
--- ios/Pods/Target Support Files/Pods-TaskLogin/Pods-TaskLogin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TaskLogin : NSObject
-@end
-@implementation PodsDummy_Pods_TaskLogin
-@end
